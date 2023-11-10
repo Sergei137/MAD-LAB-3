19acde39c987fdae578801027c37ce34ce3c6ba5
Sergei137 <sm1.618@protonmail.com>
2023-11-09 23:36:48 -0700

    lab 3 done

---
+++ App.jsx
@@ -0,0 +1,44 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, ScrollView } from 'react-native';
+
+import ToDoList from './ToDoList'; // import ToDoList component
+import ToDoForm from './ToDoForm'; // import ToDoForm component
+
+function App() {
+  const [tasks, setTasks] = React.useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+  //setTasks([...tasks, 'New task']);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <ToDoList tasks={tasks} />
+      </ScrollView>
+      <View style={styles.form}>
+        <ToDoForm />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+});
+
+export default App;
